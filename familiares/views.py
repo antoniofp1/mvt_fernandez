d2355da0c7d16f8eab0bfbb8ddae9ff81682b801
antoniofp1 <antoniofernandez158@gmail.com>
2022-08-28 21:58:38 +0200

    actualizacion 4.0

--- familiares/views.py
+++ familiares/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.template import Template, Context, loader
-from familiares.models import per_familia
+from familiares.models import pr_familia
 
 def probar_template(request):
-    queryset = per_familia.objects.all()
+    queryset = pr_familia.objects.all()
     diccionario = {'familiares': queryset}
     plantilla = loader.get_template( 'familiares_list.html')
     documento_html = plantilla.render(diccionario)
