cc19546c6079804e7ff1f73e38d1a7df7d242b84
antoniofp1 <antoniofernandez158@gmail.com>
2022-08-28 22:12:20 +0200

    actualizacion 6.0

--- familiares/views.py
+++ familiares/views.py
@@ -5,7 +5,7 @@ from familiares.models import pr_familia
 def probar_template(request):
     queryset = pr_familia.objects.all()
     diccionario = {'familiares': queryset}
-    plantilla = loader.get_template( 'familiares_list.html')
+    plantilla = loader.get_template('familiares_list.html')
     documento_html = plantilla.render(diccionario)
 
     return HttpResponse(documento_html)
